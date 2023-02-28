3442b4953c68992d6ae7902168f8f77051d05e40
s0man <maharjansoman@yahoo.com>
2023-02-28 20:07:19 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <section class="px-4 pt-5 pb-4 space-y-4">
-        <h2 class="center font-mono text-4xl">Todos</h2>
+        <h2 class="center font-mono text-4xl">Todos </h2>
         <div>
           <Todo />
         </div>
